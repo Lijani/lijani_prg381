cf47dc003579e15996bf9105b4e1c5c1d0c4182c
Lijani <lijanivwdv@gmail.com>
2021-02-24 10:34:10 +0200

    chane #2

--- prg381.java
+++ prg381.java
@@ -3,6 +3,8 @@ public class prg381{
     public static void main(String[] args) {
         System.out.println("first java git file to submit");
         //comment
+        //comment
+        
     }
 
 }
